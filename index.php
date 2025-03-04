3365c56406ee71ba1fc683d8a975a415c96b13bb
Piotr Floryn <15006@eans-nt.edu.pl>
2025-03-04 12:53:31 +0100

    convert index.html to index php with dynamic menu and content

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
     <title>Pan Tadeusz</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -16,15 +15,26 @@
             <div class="col-4">
                 <div class="list-group">
                     <a href="./" class="list-group-item list-group-item-action">Strona główna</a>
-                    <?php
-                        for ($k=1; $k <= 12; $k++) {
-                            // echo '<a href="./k$k.html" class="list-group-item list-group-item-action">k$</a>';
-                            echo "<a href='./k$k.html' class='list-group-item list-group-item-action'>Księga $k</a>";
-                        }
+                    <?php 
+                    for ($i=1;$i<=12;$i++){
+                        $active = isset($_GET['k']) && $_GET['k'] == $i ? "active" : "";
+                        echo("<a href='./index.php?k=$i' class='list-group-item list-group-item-action $active'>Księga $i</a>");
+                    }
                     ?>
+
                 </div>
             </div>
-            <div class="col-8"></div>
+            <div class="col-8">
+                  <?php
+                    if(isset($_GET['k'])){
+                        $k=$_GET['k'];
+                        include_once("./k".$k.".html");
+                    }else{
+                        echo("");
+                    }
+                ?>
+                <img src="./pantadeusz.png" alt="PanTadeusz" class="img-fluid img-thumbnail">
+            </div>
         </div>
     </section>
     <footer class="container">
